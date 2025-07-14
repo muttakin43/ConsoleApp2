4465f8167c2e6ee659208ba42f4cb075f37de2c2
muttakin43 <eamsiam6@gmail.com>
2025-07-14 15:28:47 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+    {
+        class Program
+        {
+
+
+            static void Main(string[] args)
+            {
+            int x;
+            int y;
+            double result;
+            try
+            {
+                Console.Write("Enter first number: ");
+                x = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter second number: ");
+                y = Convert.ToInt32(Console.ReadLine());
+
+                result = x / y;
+                Console.WriteLine("Result: " + result);
+            } 
+            catch (FormatException e)
+            {
+                Console.WriteLine("Enter only numbers please!");
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine("you can't divide by zero IDIOT!");
+            }
+            finally
+            {
+                Console.WriteLine("Thanks for visiting");
+            }
+           
+            Console.ReadLine();
+
+        }
+        }
+    }
+
+
